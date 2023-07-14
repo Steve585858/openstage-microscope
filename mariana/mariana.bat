b4c8e60bdafae46f763e2c1cf86c2d35f5ae00e6
Steve585858 <stevenpei99@gmail.com>
2023-07-14 11:58:35 -0500

    update bat

--- mariana/mariana.bat
+++ mariana/mariana.bat
@@ -28,27 +28,28 @@ SET RESOURCESPATH=%LAUNCH_HOME%resources
 SET HISTORYPATH=%LAUNCH_HOME%history
 
 rem echo %CLASSPATH%
-SET /A tool_id=%1
-SET INPUTFILE=%~f2
+rem SET /A tool_id=%1
+rem SET INPUTFILE=%~f2
 IF "%1%"=="" goto GUI
 IF "%1%"=="0" goto QA
 
 echo invalid argument.
 goto Exit
+
 :GUI
-rem & C:/Python311/python.exe c:/Projects/books/sandbox/camera1.py
-rem C:/Python311/python.exe %LAUNCH_HOME%camera1.py
-python.exe %LAUNCH_HOME%src\MarianaGUI.py
-PAUSE
-goto Exit
+	rem & C:/Python311/python.exe c:/Projects/books/sandbox/camera1.py
+	rem C:/Python311/python.exe %LAUNCH_HOME%camera1.py
+	python.exe %LAUNCH_HOME%src\MarianaGUI.py
+	PAUSE
+	goto Exit
 
 :QA
-SET DICTIONARYFILE=%~f2
-SET /A project_id=%3
-echo QA= %tool_id% %INPUTFILE% %DICTIONARYFILE% %project_id%
-%LAUNCH_HOME%jre\bin\java -d64 -Xms512m -Xmx4096m -showversion -Drtb.dir=%LAUNCH_HOME% ^
-	-Djava.library.path=%LIBRARYPATH% -Djava.resources.path=%RESOURCESPATH% -Djava.history.path=%HISTORYPATH% ^
-	-cp %CLASSPATH% edu.uth.app.qac.QacApp %tool_id% %INPUTFILE% %DICTIONARYFILE% %project_id%
-goto Exit
+	SET DICTIONARYFILE=%~f2
+	SET /A project_id=%3
+	echo QA= %tool_id% %INPUTFILE% %DICTIONARYFILE% %project_id%
+	%LAUNCH_HOME%jre\bin\java -d64 -Xms512m -Xmx4096m -showversion -Drtb.dir=%LAUNCH_HOME% ^
+		-Djava.library.path=%LIBRARYPATH% -Djava.resources.path=%RESOURCESPATH% -Djava.history.path=%HISTORYPATH% ^
+		-cp %CLASSPATH% edu.uth.app.qac.QacApp %tool_id% %INPUTFILE% %DICTIONARYFILE% %project_id%
+	goto Exit
 
 :Exit
